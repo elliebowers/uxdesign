d37ac4b5293c8f5f91fd9280852c9fe73e23eed9
elliebowers <ebowersche@brynmawr.edu>
2021-06-28 15:31:23 -0500

    setup for image

---
+++ script.js
@@ -0,0 +1,13 @@
+function before () {
+    document.getElementById('')
+    .src = "";
+    document.getElementById('')
+
+}
+
+function afterr () {
+    document.getElementById('')
+    .src = "";
+    document.getElementById('')
+s
+}
