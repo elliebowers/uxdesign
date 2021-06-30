308a4a1996624ccefab27215dbb06a80ff5c807b
elliebowers <ebowersche@brynmawr.edu>
2021-06-29 21:01:03 -0500

    heading and subheading

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ informationForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
 
-    var nameValue = document.getElementById("h1").value;
-    document.getElementById("heading").innerHTML = nameValue;
-    console.log('Form submitted')
+    //var nameValue = document.getElementById("h1").value;
+  console.log(event.target)
+    const formData = new FormData(event.target);
+    console.log(formData)
+    const formProps = Object.fromEntries(formData);
+    console.log(formProps);
+ 
+    document.getElementById("chosenheading").innerHTML = formProps.heading;
+    document.getElementById("chosensubheading").innerHTML = formProps.subheading;
+    console.log('Form submitted');
     
 })
-
 
